c546d40ad67ec4fb3044f1a20075d2b42c3eb152
Junior Oliveira <arojunior@gmail.com>
2017-05-30 16:50:43 -0300

    select component added

--- src/index.js
+++ src/index.js
@@ -9,34 +9,66 @@ const RenderField = ({
   inputClass,
   labelClass,
   divClass,
+  className,
   type,
+  selectOptions,
   meta: {touched, error}
-}) => (
-  <div className={divClass}>
-    {label &&
-      <div className={labelClass}>
-        <label>{label}</label>
-      </div>}
-    <div className={inputClass}>
-      {type === 'textarea'
-        ? <textarea
-            {...input}
-            placeholder={placeholder}
-            type={type}
-            className="form-control"
-          />
-        : <input
-            {...input}
-            placeholder={placeholder}
-            type={type}
-            className="form-control"
-          />}
+}) => {
+  const inputComponent = (
+    <input
+      placeholder={placeholder}
+      type={type}
+      className={className}
+      {...input}
+    />
+  )
 
+  const textareaComponent = (
+    <textarea
+      placeholder={placeholder}
+      type={type}
+      className={className}
+      {...input}
+    />
+  )
+
+  const selectComponent = () => {
+    const options = selectOptions
+      ? selectOptions.map((option, key) => (
+          <option value={option.value} key={key}>{option.name}</option>
+        ))
+      : null
+    return (
+      <select className={className} {...input}>
+        {options}
+      </select>
+    )
+  }
+
+  const renderComponent = () => {
+    switch (type) {
+      case 'textarea':
+        return textareaComponent
+      case 'select':
+        return selectComponent()
+      default:
+        return inputComponent
+    }
+  }
+
+  return (
+    <div className={divClass}>
+      {label &&
+        <div className={labelClass}>
+          <label>{label}</label>
+        </div>}
+      <div className={inputClass}>
+        {renderComponent()}
+      </div>
+      {touched && (error && <span className="text-danger">{error}</span>)}
     </div>
-    {touched && (error && <span className="text-danger">{error}</span>)}
-  </div>
-)
-
+  )
+}
 export default RenderField
 
 /*
@@ -45,17 +77,15 @@ export default RenderField
 export const required = value => (value ? undefined : 'Required')
 
 export const maxLength = max => value =>
-  (value && value.length > max
-    ? `Must be ${max} characters or less`
-    : undefined)
+  value && value.length > max ? `Must be ${max} characters or less` : undefined
 
 export const number = value =>
-  (value && isNaN(Number(value)) ? 'Must be a number' : undefined)
+  value && isNaN(Number(value)) ? 'Must be a number' : undefined
 
 export const minValue = min => value =>
-  (value && value < min ? `Must be at least ${min}` : undefined)
+  value && value < min ? `Must be at least ${min}` : undefined
 
 export const email = value =>
-  (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
     ? 'Invalid email address'
-    : undefined)
+    : undefined
